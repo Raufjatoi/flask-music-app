6edeac85b5024e508302e09f690fc484b1ad1469
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-04-07 11:41:12 +0500

    created albums.html and static2
    also updated others based on the changes io made for creating albums sec tom do more in tha sec

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+
+@app.route('/static2/<path:filename>')
+def static2_files(filename):
+    return send_from_directory('static2', filename)
 
 @app.route('/')
 def index():
@@ -10,6 +15,9 @@ def index():
 def discover():
     return render_template('discover.html')
 
+@app.route('/albums')
+def albums():
+    return render_template('albums.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
