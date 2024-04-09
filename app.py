d6bb59356d6768aa694b38944d80259141dbfd3f
Abdul Rauf <141711142+Raufjatoi@users.noreply.github.com>
2024-04-09 11:20:37 +0500

    created artist.html and updated others
    created artist.html and updated others respectively and also try somethin to make it cool but failed (kinda)

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-
 @app.route('/static2/<path:filename>')
 def static2_files(filename):
     return send_from_directory('static2', filename)
+
+@app.route('/static3/<path:filename>')
+def static3_files(filename):
+    return send_from_directory('static3', filename)
 
 @app.route('/')
 def index():
@@ -18,6 +21,11 @@ def discover():
 @app.route('/albums')
 def albums():
     return render_template('albums.html')
+
+@app.route('/artist')
+def artist():
+    # You can add logic here to fetch data for artist.html if needed
+    return render_template('artist.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
